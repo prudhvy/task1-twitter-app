34893cddf7137ecc551144120a72eaacffbbf54f
thekingaziz <abdulaazeez786@gmail.com>
2018-01-03 09:51:43 +0530

    SideBar necessary import statement written.

---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Body,Card,CardItem,Content,Container,Deckswiper,Footer,Header,Icon,ListItem,List,Left,Right,Text,Thumbnail,View} from 'native-base';
+import {Image,StatusBar,Settings } from "react-native";
+import {StackNavigator} from 'react-navigation';
+export default class SideBar extends React.Component 
+{
+    render() 
+    {
+        return ();
+    }
+}
+  
