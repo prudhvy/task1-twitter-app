7330a8ec20206e3f1be4de9001ee91e68908e3b4
thekingaziz <abdulaazeez786@gmail.com>
2018-01-03 10:06:30 +0530

    SideBar and MainScreen Completed.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,6 +9,59 @@ export default class SideBar extends React.Component
         return 
         (
         <Container>
+            <Content>
+                <Card>
+                    <CardItem >
+                        <View>
+                            <Thumbnail source={require('../images/1.png')} />
+                            <Text style={{fontWeight: 'bold'}}>Abdul Azeez K <Icon name="arrow-dropdown" style={{color: '#3BB9FF'}}/></Text>
+                            <Text note>@abdulazeezk</Text>
+                        <View style = {{flexDirection: 'row'}} >
+                            <Text>100 Following </Text>
+                            <Text>50 Followers</Text>
+                        </View>
+                        </View>
+                    </CardItem>
+                    <CardItem style={{elevation: 2}}>
+                        <List>
+                            <ListItem button noborder icon>
+                                <Left>
+                                    <Icon name='person'style={{color: '#3BB9FF'}}/>
+                                        <Text>Profile</Text>
+                                </Left>
+                            </ListItem>
+                            <ListItem button noborder icon>
+                                <Left>
+                                    <Icon name='list-box' style={{color: '#3BB9FF'}}/>
+                                        <Text>Lists</Text>
+                                </Left>
+                            </ListItem>
+                            <ListItem button noborder icon>
+                                <Left>
+                                    <Icon name='flash' style={{color: '#3BB9FF'}}/>
+                                        <Text>Flash</Text>
+                                </Left>
+                            </ListItem>
+                            <ListItem button noborder icon>
+                                <Left>
+                                    <Icon name='albums' style={{color: '#3BB9FF'}}/>
+                                        <Text>Albums</Text>
+                                </Left>
+                            </ListItem>
+                        </List>
+                    </CardItem>
+                    <CardItem >
+                        <List>
+                            <ListItem button noborder>
+                                <Text>Settings and Privacy</Text>               
+                            </ListItem>
+                            <ListItem button noborder>
+                                <Text>Help Center</Text>               
+                            </ListItem>
+                        </List>
+                    </CardItem>
+                </Card>      
+            </Content>
             <Footer style={{backgroundColor:"white"}} >
                 <Left>
                     <Icon style={{ marginLeft: 20,color: '#3BB9FF'}} name='moon' ></Icon>
